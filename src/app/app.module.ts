4690d17acb9d2a12730a57a20d7b3d6001998fdb
John Ajiboye <ajiboye.o.john@gmail.com>
2018-07-08 21:03:05 +0300

    Added data/services ||Updated Header/footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MoviesComponent } from './movies/movies.component';
-import { MovieDetailsComponent } from './movie-details/movie-details.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes: Routes = [
+  { path: 'movie/:id', component: MoviesComponent },
+  {
+    path: 'movie',
+    component: MovieDetailsComponent,
+    data: { title: 'Movies List' }
+  },
+  {
+    path: '',
+    redirectTo: '/movies',
+    pathMatch: 'full'
+  },
+  { path: '**', component: MoviesComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
     MoviesComponent,
     MovieDetailsComponent,
     HeaderComponent,
     FooterComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  // providers: [MoviesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
